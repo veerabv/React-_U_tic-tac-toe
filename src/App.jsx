e22b2437f9b11e4030a86e40701f21b7700575b2
veerabv <bveera0608@gmail.com>
2024-06-25 12:38:37 +0530

    finding the winner

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function derivedActivePlayer(turns) {
 }
 
 function App() {
+  let winner;
   const [gameTurns, setGameTurns] = useState([])
    
   const activePlayer = derivedActivePlayer(gameTurns);
@@ -32,7 +33,14 @@ function App() {
 
     gameBoard[row][column] = player;
   }
-  
+  for(const combination of WINNING_COMBINATIONS){
+    let firstSquare = gameBoard[combination[0].row][combination[0].column];
+    let secondSquare = gameBoard[combination[1].row][combination[1].column];
+    let thirdSquare = gameBoard[combination[2].row][combination[2].column];
+    if(firstSquare && firstSquare === secondSquare && firstSquare === thirdSquare){
+      winner = firstSquare;
+    }
+  }
 
  
   function handleSquare(row,col){
@@ -57,6 +65,7 @@ function App() {
           <Player name = "Player2" symbol = "O" className = {activePlayer=== "O" ? "active":""} />
 
           </ol>
+          {winner && <p>You won , {winner}</p>}
           <GameBoard board = {gameBoard} handleSquare = {handleSquare}/>
         </div>
         <Log turns = {gameTurns}/>
